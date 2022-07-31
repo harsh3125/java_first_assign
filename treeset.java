4519f8ecbf5f736e55baf9f50fb9fe1d39a27dcc
harsh3125 <55591116+harsh3125@users.noreply.github.com>
2022-07-31 16:59:38 +0530

    Create treeset.java

---
+++ treeset.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class treeset {
+	public static void main(String[] args)
+	{
+		NavigableSet<String> ts = new TreeSet<>();
+		ts.add("Harsh");
+		ts.add("is");
+		ts.add("learning");
+		ts.add("java");
+		System.out.println("Initial TreeSet " + ts);
+	}
+}
