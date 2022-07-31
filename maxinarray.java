4e4cd7330a8bc57319a242fab5952c022e6a51a3
harsh3125 <55591116+harsh3125@users.noreply.github.com>
2022-07-31 16:52:10 +0530

    Create maxinarray.java

---
+++ maxinarray.java
@@ -0,0 +1,15 @@
+public class maxin {
+    public static int findMaxRec(int A[], int n)
+    {
+      if(n == 1)
+        return A[0];
+         
+        return Math.max(A[n-1], findMaxRec(A, n-1));
+    }
+    public static void main(String args[])
+    {
+        int A[] = {1, 4, 45, 6, -50, 10, 2};
+        int n = A.length;
+        System.out.println(findMaxRec(A, n));
+    }
+}
